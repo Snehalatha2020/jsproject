f31dc7162b9d1113b68e4a4d5cc2c39cbc09965d
Snehalatha2020 <122106778+Snehalatha2020@users.noreply.github.com>
2024-08-12 18:09:02 +0000

    container config done

---
+++ myproj/variables.js
@@ -0,0 +1,15 @@
+const accountId =1432
+let accountEmail ="athar@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not valid or allowed
+accountEmail ="atr@gmail.com"
+accountPassword = "124"
+
+/* Prefer not to use var because of issue of block scope and functional scope*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
