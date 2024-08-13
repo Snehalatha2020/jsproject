bfc8734b4399176a776bcc0a8f527b81565db3a1
Snehalatha2020 <122106778+Snehalatha2020@users.noreply.github.com>
2024-08-13 17:54:12 +0000

    container console done

---
+++ myproj/converopt4.js
@@ -0,0 +1,11 @@
+let score = "50aa"
+console.log(typeof score) 
+//for checking the type of variable
+console.log(typeof (score) );
+//sec method for checking type 
+let valueInNum = Number(score)//converts string to number
+console.log(typeof valueInNum);
+console.log(valueInNum);//to check what is in the valueInNum
+
+
+
---
+++ myproj/datatype_3.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newwr version of JS
+// alert ( 3 + 3) we are using node js not browser
+// console.log( 3 + 3 ); console.log("senu") code readability should be high
+console.log("senu")
+console.log( 3 + 3);
+
+let name = "snehal" //string datatype
+let age = 20 //number
+let isLoggedin = false //boolean
+let state = null //null
+let stat; //undefined
+
+
+console.log(typeof "senu"); //string as output
+console.log(typeof age);// numder as output
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
+
