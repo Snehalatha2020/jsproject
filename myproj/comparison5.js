9ba3d1b86adda4259c81d34def638c8b0758ec71
Snehalatha2020 <122106778+Snehalatha2020@users.noreply.github.com>
2024-08-15 05:27:11 +0000

    comparison

---
+++ myproj/comparison5.js
@@ -0,0 +1,16 @@
+console.log( 2 > 1)//true
+console.log( 2 >= 1)//true
+console.log( 2 < 1)//false
+console.log( 2 == 1)//false
+console.log( 2 != 1)//true
+
+console.log( "2" > 1);//true but no predictable result
+console.log("02" > 1);//true but no predictable result
+
+console.log( null > 0)//false
+console.log( null == 0)//false
+console.log( null >= 0)//true as there is problem in conversion
+
+console.log( "2" === 2)
+
+
