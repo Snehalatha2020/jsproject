85dba61d2cdf630f059a220738bf6cff0a862328
Snehalatha2020 <122106778+Snehalatha2020@users.noreply.github.com>
2024-08-15 04:16:11 +0000

    Conversion of datatypes

--- myproj/converopt4.js
+++ myproj/converopt4.js
@@ -1,11 +1,29 @@
-let score = "50aa"
+let score = true
 console.log(typeof score) 
 //for checking the type of variable
 console.log(typeof (score) );
 //sec method for checking type 
 let valueInNum = Number(score)//converts string to number
 console.log(typeof valueInNum);
-console.log(valueInNum);//to check what is in the valueInNum
+console.log(valueInNum);//to check what is in the value in Number
+//=>(conversion to number)
+// "33" => 33
+//"33aabb" => NaN(type of NaN is number)
+// true => 1
+//false => 0
 
+let islogin = 1
+let booleanlogin = Boolean(islogin)
+console.log(booleanlogin);
+//=>(conversion to boolean)
+// 1 => true
+// 0 => false
+// ""(empty string) => false
+// "value" => true
+
+let somenum = 33
+let randomnum = String(somenum)
+console.log(randomnum);
+console.log(typeof randomnum);
 
 
